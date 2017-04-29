4b6677ed19b290a8f21b7d311654b71238661c03
Matthew J. Gunton <mgunton18@lawrenceville.org>
2017-04-29 15:50:32 -0400

    w

---
+++ JS_Files/display.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function(){
+  var config = {
+          apiKey: "AIzaSyBjC0LvxiL8Epg-YO5ohd2ZBb_CdbkI8h4",
+          authDomain: "reshwaptest.firebaseapp.com",
+          databaseURL: "https://reshwaptest.firebaseio.com",
+          storageBucket: "reshwaptest.appspot.com",
+          messagingSenderId: "1083636691406"
+      };
+    firebase.initializeApp(config);
+});
+
+function testing(){
+
+var cat = $("#catChooser").val();
+var ref = firebase.database().ref("forms");
+  console.log("option "+cat);
+
+ref.orderByKey().endAt("1000").on("child_added", function(snapshot){
+  var option = snapshot.val();
+  console.log(option.category);
+
+  $("#FormArea").append("<div class= '"+option.category+" box'>"+option.contactEmail+"</div><br>");
+
+//give the option to hide things
+
+  if(cat == "Books"){
+    $(".Electronics").hide();
+    $(".Furniture").hide();
+    $(".Other").hide();
+  }
+  else if(cat == "Electronics"){
+    $(".Books").hide();
+    $(".Furniture").hide();
+    $(".Other").hide();
+  }
+  else if(cat == "Furniture"){
+    $(".Electronics").hide();
+    $(".Books").hide();
+    $(".Other").hide();
+  }
+  else if(cat == "Other"){
+    $(".Electronics").hide();
+    $(".Furniture").hide();
+    $(".Books").hide();
+  }
+  else{
+    console.log("give em everything");
+  }
+
+  });
+};
