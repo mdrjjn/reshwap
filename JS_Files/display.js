57af714afb5bf343d1051c75515c81a3637ff494
Max Drojjin <max.drojjin@gmail.com>
2017-05-06 20:37:05 -0400

    started the display form

--- JS_Files/display.js
+++ JS_Files/display.js
@@ -10,43 +10,50 @@ $(document).ready(function(){
     firebase.initializeApp(config);
 });
 
-function testing(){
+function findAndDisplay(category){
+  var ref = firebase.database().ref("forms");
 
-var cat = $("#catChooser").val();
-var ref = firebase.database().ref("forms");
-  console.log("option "+cat);
-
-ref.orderByKey().endAt("1000").on("child_added", function(snapshot){
-  var option = snapshot.val();
-  console.log(option.category);
-
-  $("#FormArea").append("<div class= '"+option.category+" box'>"+option.contactEmail+"</div><br>");
-
-//give the option to hide things
-
-  if(cat == "Books"){
-    $(".Electronics").hide();
-    $(".Furniture").hide();
-    $(".Other").hide();
-  }
-  else if(cat == "Electronics"){
-    $(".Books").hide();
-    $(".Furniture").hide();
-    $(".Other").hide();
-  }
-  else if(cat == "Furniture"){
-    $(".Electronics").hide();
-    $(".Books").hide();
-    $(".Other").hide();
-  }
-  else if(cat == "Other"){
-    $(".Electronics").hide();
-    $(".Furniture").hide();
-    $(".Books").hide();
-  }
-  else{
-    console.log("give em everything");
-  }
-
+  ref.orderByChild('category').equalTo(category).on("child_added", function(snapshot) {
+    console.log(snapshot.val().title);
   });
+
 };
+
+
+// var cat = $("#catChooser").val();
+// var ref = firebase.database().ref("forms");
+//   console.log("option "+cat);
+//
+// ref.orderByKey().endAt("1000").on("child_added", function(snapshot){
+//   var option = snapshot.val();
+//   console.log(option.category);
+//
+//   $("#FormArea").append("<div class= '"+option.category+" box'>"+option.contactEmail+"</div><br>");
+//
+// //give the option to hide things
+//
+//   if(cat == "Books"){
+//     $(".Electronics").hide();
+//     $(".Furniture").hide();
+//     $(".Other").hide();
+//   }
+//   else if(cat == "Electronics"){
+//     $(".Books").hide();
+//     $(".Furniture").hide();
+//     $(".Other").hide();
+//   }
+//   else if(cat == "Furniture"){
+//     $(".Electronics").hide();
+//     $(".Books").hide();
+//     $(".Other").hide();
+//   }
+//   else if(cat == "Other"){
+//     $(".Electronics").hide();
+//     $(".Furniture").hide();
+//     $(".Books").hide();
+//   }
+//   else{
+//     console.log("give em everything");
+//   }
+//
+//   });
